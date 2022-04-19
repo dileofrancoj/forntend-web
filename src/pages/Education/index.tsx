d6d1e93e41d0dca7d77732520f2a601b56420dfa
Franco Di Leo <dileo.francoj@gmail.com>
2022-04-19 10:52:33 -0300

    feat: add education

--- src/pages/Education/index.tsx
+++ src/pages/Education/index.tsx
@@ -2,8 +2,20 @@ import MarkdownPreview from '@uiw/react-markdown-preview'
 
 function Education() {
   const source = `
-    # Education
+  # Academic training
+  ## UTN (2012 - 2018)
+  ### Mechanical Engineer
+ ## Universidad de Palermo
   
+  ### Bachelor of Mathematics (2021 - Current)
+  ## Courses
+  <ul>
+  <li> Initial Web Developer (UTN)</li>
+  <li> Advanced Web Developer (UTN)</li>
+  <li> React JS (UTN)</li>
+  <li> Algorithms and Data structures (UTN)</li>
+  <li> IoT (UTN)</li>
+  </ul>   
   `
   return <MarkdownPreview source={source} className="fullContainer" />
 }
