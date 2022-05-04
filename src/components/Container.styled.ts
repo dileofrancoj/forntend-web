5d4b91db5f30d71a302155e8a7e1f4f14873c42c
Franco Di Leo <dileo.francoj@gmail.com>
2022-05-04 08:27:27 -0300

    feat: about me stuff

---
+++ src/components/Container.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 2em 3em;
+  box-sizing: border-box;
+  background: #0d1117;
+  color: #c9d1d9;
+`;
