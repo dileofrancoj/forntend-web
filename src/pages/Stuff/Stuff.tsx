5f964ff26804872885b73e3f24b333ab0e4207de
Franco Di Leo <dileo.francoj@gmail.com>
2022-05-16 15:10:20 -0300

    fix: image fluid

--- src/pages/Stuff/Stuff.tsx
+++ src/pages/Stuff/Stuff.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import { Container } from '../../components/Container.styled'
 import Footer from '../../components/Footer';
 
@@ -12,7 +12,11 @@ const Stuff = () => {
                         <h1>Sobre mi</h1>
                         <p >Si llegaste hasta acá, te voy a contar un poco más acerca mio pero en español (ya que es mi lengua nativa) y sin formato markdown para que sea mas ameno.</p>
                         <p >Antes de seguir con otras cosas, te quiero mostrar mi setup.</p>
-                        <img src="https://preview.redd.it/3bzwmbgzpno71.jpg?width=965&format=pjpg&auto=webp&s=90b32bfedc22f55a2783e0e93931c39f403a8e80" alt="Setup" />
+                        <Image rounded fluid src="https://preview.redd.it/3bzwmbgzpno71.jpg?width=965&format=pjpg&auto=webp&s=90b32bfedc22f55a2783e0e93931c39f403a8e80" alt="Setup" />
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center mt-4">
+                    <Col md={6} xs={12}>
                         <p>Al pasar tantas horas del día dentro de la oficina, es super importante para mi estar cómodo y tener buenas herramientas de trabajo para afrontar el día a día.</p>
                         <p>Por eso cuento con:</p>
                         <ul>
